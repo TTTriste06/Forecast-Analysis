296350fff9ec54387464af201996281dac458796
TTTriste06 <yunhan.yang08@gmail.com>
2025-07-03 10:38:44 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -70,10 +70,6 @@ class PivotProcessor:
         # Step 1: 读取主计划模板
         main_df = template_file[["晶圆", "规格", "品名"]].copy()
         main_df.columns = ["晶圆品名", "规格", "品名"]
-
-        st.write(forecast_file)
-        st.write(order_file)
-        st.write(sales_file)
         
         FIELD_MAPPINGS = {
             "forecast": {"品名": "生产料号"},
