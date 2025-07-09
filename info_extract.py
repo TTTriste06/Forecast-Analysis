b9eb5bcab3259f589be8b8b38c9e08bfa4ff0688
TTTriste06 <yunhan.yang08@gmail.com>
2025-07-09 15:25:14 +0800

    Update info_extract.py

--- info_extract.py
+++ info_extract.py
@@ -7,14 +7,16 @@ from openpyxl.styles import PatternFill
 
 
 def extract_all_year_months(df_forecast, df_order, df_sales):
-    # 1. 从 forecast header 提取 x月预测 列中的月份
+     # 1. 从 forecast header 提取 x月预测 列中的月份
+    if forecast_year is None:
+        forecast_year = datetime.today().year
     month_pattern = re.compile(r"(\d{1,2})月预测")
     forecast_months = []
     for col in df_forecast.columns:
         match = month_pattern.match(str(col))
         if match:
             month = match.group(1).zfill(2)
-            forecast_months.append(f"2025-{month}")  # ✅ 根据需要调整年份
+            forecast_months.append(f"{forecast_year}-{month}")  # ✅ 根据需要调整年份
 
     # 2. 从 order 文件第 B 列（假设是“订单日期”）
     order_date_col = df_order.columns[11]
